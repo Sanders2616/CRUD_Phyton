ae11d5dfeda24640733570bf16b8f9b031d7d68a
Sanders2616 <sandra.aguilarsantos@utxicotepec.edu.mx>
2023-07-11 16:24:55 -0600

    1107

--- routes/alumno.py
+++ routes/alumno.py
@@ -2,29 +2,75 @@ from fastapi import APIRouter
 from config.db import engine, conn
 from models.alumno import alumnos
 from schemas.alumno import Alumno
-router=APIRouter()
 
-#Servicio para aobtener todos los alumnos
+router = APIRouter()
+
 @router.get('/getAll')
-def ObtenerAlumnos():
-    result=conn.execute(alumnos.select()).fetchall()
-    response=[]
+def obtenerAlumnos():
+    result = conn.execute(alumnos.select()).fetchall()
+    response = []
     for tuple in result:
-        alumno={
+        alumno = {
             "matricula":tuple[0],
             "nombre":tuple[1],
             "apellidos":tuple[2],
             "cuatrimestre":tuple[3],
-            "promedio":tuple[4]
+            "promedio":tuple[4],
         }
         response.append(alumno)
-        return response
+    return response
 
 @router.post('/insert')
 def insertAlumno(alumno:Alumno):
     conn.execute(alumnos.insert().values(dict(alumno)))
     conn.commit()
     res={
-        "status":"Alumno Insertado Con Exito!"
+        "status":"Alumno insertado con éxito"
     }
+    return res 
+
+@router.get('/getOne/{matricula}')
+def obtenerAlumno(matricula:int):
+    student_tuple=conn.execute(alumnos.select().where(alumnos.c.matricula==matricula)).first()
+    if student_tuple!=None:
+        student_dict={
+                "matricula":student_tuple[0],
+                "nombre":student_tuple[1],
+                "apellidos":student_tuple[2],
+                "cuatrimestre":student_tuple[3],
+                "promedio":student_tuple[4],
+        }
+        return student_dict
+    else:
+        res={
+            "status":"Alumno no encontrado"
+        }
+        return res
+    
+
+@router.put('/updateOne/{matricula}')
+def actualizarAlumno(alumno:Alumno,matricula):
+    res=obtenerAlumno(matricula)
+    print(res)
+    if res.get("status")=="No existe el alumno":
+        return res
+    else:
+        result=conn.execute(alumnos.update().values(dict(alumno)).where(alumnos.c.matricula==matricula)).last_updated_params()
+        conn.commit()
+    return result
+
+
+
+@router.delete('/deleteone/{matricula}')
+def eliminarAlumno(matricula):
+     res=obtenerAlumno(matricula)
+    print(res)
+    if res.get("status")=="No existe el alumno":
+        return res
+    else:
+        result=conn.execute(alumnos.delete().where(alumnos.c.matricula==matricula))
+        conn.commit()
+        res={
+            "status":"Alumno eliminado"
+        }
     return res
