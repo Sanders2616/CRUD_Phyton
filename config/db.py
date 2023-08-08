ffafd9d2f2bde8df5a9dc1842b464bf43d450c80
Sanders2616 <sandra.aguilarsantos@utxicotepec.edu.mx>
2023-08-08 16:07:15 -0600

    c

--- config/db.py
+++ config/db.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine,MetaData
-engine=create_engine("mysql+pymysql://root:Sandra26@localhost:3306/utxjdb")
+engine=create_engine("mysql+pymysql://root:1234@localhost:3307/utxjdb")
 meta=MetaData()
 conn=engine.connect()
